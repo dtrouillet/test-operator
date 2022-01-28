63dc94faf4354cd9df77db9a51901509441db6f2
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2022-01-28 14:20:23 +0100

    fix(rbac): add permission

--- controllers/site_controller.go
+++ controllers/site_controller.go
@@ -41,6 +41,9 @@ type SiteReconciler struct {
 //+kubebuilder:rbac:groups=test.faya.fr,resources=sites,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=test.faya.fr,resources=sites/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=test.faya.fr,resources=sites/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking.k8s.io,resources=ingress,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
